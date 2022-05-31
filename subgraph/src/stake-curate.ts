783140d65d6d6a3be14236a7066f848506e79658
Green <40367733+greenlucid@users.noreply.github.com>
2022-05-31 03:56:28 +0100

    feat: handle account creation

--- subgraph/src/stake-curate.ts
+++ subgraph/src/stake-curate.ts
@@ -1,6 +1,7 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   StakeCurate,
+  StakeCurateCreated,
   AccountCreated,
   AccountFunded,
   AccountStartWithdraw,
@@ -20,62 +21,41 @@ import {
   MetaEvidence,
   Ruling
 } from "../generated/StakeCurate/StakeCurate"
-import { ExampleEntity } from "../generated/schema"
+import { Account, GeneralCounter } from "../generated/schema"
+import { decompress } from "./cint32"
 
-export function handleAccountCreated(event: AccountCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleStakeCurateCreated(event: StakeCurateCreated): void {
+  let counter = GeneralCounter.load("0")
+  if (!counter) {
+    counter = new GeneralCounter("0")
+    counter.accountCount = BigInt.fromI32(0)
+    counter.listCount = BigInt.fromI32(0)
+    counter.arbitrationSettingCount = BigInt.fromI32(0)
+    counter.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count.plus(BigInt.fromI32(1))
-
-  // Entity fields can be set based on event parameters
-  entity._accountId = event.params._accountId
-  entity._owner = event.params._owner
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ACCOUNT_WITHDRAW_PERIOD(...)
-  // - contract.accountCount(...)
-  // - contract.accounts(...)
-  // - contract.arbitrationSettingCount(...)
-  // - contract.arbitrationSettings(...)
-  // - contract.arbitratorAndDisputeIdToDisputeSlot(...)
-  // - contract.disputes(...)
-  // - contract.getEvidenceGroupId(...)
-  // - contract.items(...)
-  // - contract.listCount(...)
-  // - contract.lists(...)
 }
 
-export function handleAccountFunded(event: AccountFunded): void {}
+export function handleAccountCreated(event: AccountCreated): void {
+  let counter = GeneralCounter.load("0") as GeneralCounter
+
+  let account = new Account(counter.accountCount.toString())
+
+  account.accountId = counter.accountCount
+  account.owner = event.params._owner
+  account.fullStake = decompress(event.params._fullStake)
+  account.freeStake = decompress(event.params._fullStake)
+  account.lockedStake = BigInt.fromI32(0)
+  account.withdrawing = false
+  account.withdrawingTimestamp = BigInt.fromI32(0)
+  account.save()
+  // increment accountCount
+  counter.accountCount = counter.accountCount.plus(BigInt.fromI32(1))
+  counter.save()
+}
+
+export function handleAccountFunded(event: AccountFunded): void {
+  let account = Account.load(event.params._accountId.toString())
+}
 
 export function handleAccountStartWithdraw(event: AccountStartWithdraw): void {}
 
